63b18491aa126c32d0d793ddea9badf97b4864ba
hisland <hisland@qq.com>
2018-10-11 22:22:08 +0800

    2018-10-11 22:22:08 normal commit

--- server-egg2/app/controller/index.js
+++ server-egg2/app/controller/index.js
@@ -58,7 +58,6 @@ class UserController extends Controller {
     let fileList = glob.sync('*', {
       cwd: queryPath,
     })
-    console.log(queryPath, fileList)
     fileList = fileList.map(function(oneFile) {
       let fileStat = fs.statSync(nodePath.join(queryPath, oneFile))
       if (fileStat.isDirectory()) {
